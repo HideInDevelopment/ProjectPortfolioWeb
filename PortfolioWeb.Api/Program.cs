357df62acb62f1ba6c213c8c316be4d04d3ef72d
HideInDevelopment <hideindevelopment96@gmail.com>
2026-06-08 20:19:45 +0200

    Add scalar and pending migration handling.

--- PortfolioWeb.Api/Program.cs
+++ PortfolioWeb.Api/Program.cs
@@ -1,14 +1,31 @@
+using Microsoft.EntityFrameworkCore;
 using PortfolioWeb.Application;
 using PortfolioWeb.Infrastructure;
+using PortfolioWeb.Infrastructure.Persistence;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddOpenApi();
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<PortfolioWebDbContext>();
+    var pendingMigrations = await dbContext.Database.GetPendingMigrationsAsync();
+
+    if (pendingMigrations.Any())
+    {
+        await dbContext.Database.MigrateAsync();
+    }
+}
+
+app.MapOpenApi();
+app.MapScalarApiReference("/scalar");
 app.MapControllers();
 
 app.Run();
